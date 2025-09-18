ba031b37bcbb0df60906f692dba63ac183445b85
anshurama <anshurama01@gmail.com>
2025-09-18 01:24:12 +0000

    imported function, set up introductory image

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
-st.title("My first web app")
-st.header("AI Club")
-st.subheader("app")
-st.write("I am in 10th grade. I wake up at 6:00. I sleep at 10:30.")
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+import pickle
+from PIL import Image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from keras.layers import GlobalAveragePooling2D
+from keras.models import Model
+IMAGE_ADDRESS = "https://media.springernature.com/full/springer-static/image/art%3A10.1038%2Fs41598-022-05282-z/MediaObjects/41598_2022_5282_Fig1_HTML.png"
+IMAGE_SIZE = (224,224)
+IMAGE_NAME = "User_image.png"
+class_label = ["Male", "Female"]
+class_label.sort()
+
